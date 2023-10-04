ddb5e6bb92e1674463aceae6540d94657415b187
Mart <marta.nikiel@yahoo.com>
2023-10-04 13:23:58 +0200

    webformatURL doesnt work

--- src/js/images.js
+++ src/js/images.js
@@ -5,9 +5,9 @@ export { images };
 axios.defaults.baseURL = 'https://pixabay.com/api/';
 const url = 'https://pixabay.com/api/';
 const API_KEY = '39809012-794bb9f85c23fb448d6e12ec5';
-const perPage = 40;
-const page = 1;
-const key = '';
+// const perPage = 40;
+// const page = 1;
+// const key = '';
 
 async function images(key, page, perPage) {
   const response = await axios.get(url, {
--- src/js/index.js
+++ src/js/index.js
@@ -6,8 +6,14 @@ import 'simplelightbox/dist/simple-lightbox.min.css';
 const form = document.querySelector('.search-form');
 const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
+const perPage = 40;
+let key = '';
+let page = 1;
 
 const lightbox = new simpleLightbox('.gallery a');
+
+form.addEventListener('submit', searchImages);
+loadMore.addEventListener('click', getMoreImages);
 
 function clear() {
   gallery.innerHTML = '';
@@ -15,11 +21,11 @@ function clear() {
 
 function searchImages(e) {
   e.preventDefault();
-  key = e.currentTarget.searchKey.vaule.trim();
+  key = e.currentTarget.searchQuery.value.trim();
   loadMore.classList.add('is-hidden');
   clear();
   if (!key) {
-    Notiflix.Notify.info('Search images');
+    Notiflix.Notify.info('Search images...');
     return;
   }
   images(key, page, perPage)
@@ -38,4 +44,56 @@ function searchImages(e) {
       }
     })
     .catch(error => console.log(error));
+}
+
+function getMoreImages() {
+  page += 1;
+  images(key, page, perPage)
+    .then(({ data }) => {
+      getGallery(data.this);
+      lightbox.refresh();
+      const allPages = Math.ceil(data.hits / perPage);
+      if (page > allPages) {
+        loadMore.classList.add('is-hidden');
+        Notiflix.Notify.failure(
+          "We're sorry, but you've reached the end of search results."
+        );
+      }
+    })
+    .catch(error => console.log(error));
+}
+
+function getGallery(data) {
+  const markup = data
+    .map(
+      ({
+        webformatURL,
+        largeImageURL,
+        tags,
+        likes,
+        views,
+        comments,
+        downloads,
+      }) => {
+        return `<div class="photo-card"> <a href="${largeImageURL}">
+                < img src = "${webformatURL}" alt = "${tags}" loading = "lazy" title="${tags}"/></a>
+                    <div class="info">
+                        <p class="info-item">
+                            <b>Likes</b>${likes}
+                        </p>
+                        <p class="info-item">
+                            <b>Views</b>${views}
+                        </p>
+                        <p class="info-item">
+                            <b>Comments</b>${comments}
+                        </p>
+                        <p class="info-item">
+                            <b>Downloads</b>${downloads}
+                        </p>
+                    </div>
+</div>`;
+      }
+    )
+    .join('');
+  gallery.insertAdjacentHTML('beforeend', markup);
 }
