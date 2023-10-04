32b65d374850d30f0c5955fc65b72eaf9d4abeae
Mart <marta.nikiel@yahoo.com>
2023-10-04 09:22:25 +0200

    added searchImages function

--- src/js/images.js
+++ src/js/images.js
@@ -6,6 +6,8 @@ axios.defaults.baseURL = 'https://pixabay.com/api/';
 const url = 'https://pixabay.com/api/';
 const API_KEY = '39809012-794bb9f85c23fb448d6e12ec5';
 const perPage = 40;
+const page = 1;
+const key = '';
 
 async function images(key, page, perPage) {
   const response = await axios.get(url, {
--- src/js/index.js
+++ src/js/index.js
@@ -8,3 +8,34 @@ const gallery = document.querySelector('.gallery');
 const loadMore = document.querySelector('.load-more');
 
 const lightbox = new simpleLightbox('.gallery a');
+
+function clear() {
+  gallery.innerHTML = '';
+}
+
+function searchImages(e) {
+  e.preventDefault();
+  key = e.currentTarget.searchKey.vaule.trim();
+  loadMore.classList.add('is-hidden');
+  clear();
+  if (!key) {
+    Notiflix.Notify.info('Search images');
+    return;
+  }
+  images(key, page, perPage)
+    .then(({ data }) => {
+      if (!data.totalHits) {
+        Notiflix.Notify.failure(
+          'Sorry, there are no images matching your search query. Please try again.'
+        );
+      } else {
+        getGallery(data.hits);
+        lightbox.refresh();
+        Notiflix.Notify.success('Hooray! We found ${data.totalHits} images.');
+        if (data.totalHits > perPage) {
+          loadMore.classList.remove('is-hidden');
+        }
+      }
+    })
+    .catch(error => console.log(error));
+}
